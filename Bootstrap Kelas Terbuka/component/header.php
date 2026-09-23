ed42882f684f863d1c866f934292e348f703d1b1
aditgaming1111111-jpg <aditgaming1111111@gmail.com>
2026-09-23 12:27:45 +0700

    Bootsratps episode 3

---
+++ Bootstrap Kelas Terbuka/component/header.php
@@ -0,0 +1,20 @@
+   
+   <section class="header bg-light text-center text-sm-start pt-5 mt-5" >
+        <div class="container">
+
+            <div class="d-flex align-items-center">
+                <div>
+                    <h1><span class="text-primary">Mari</span> Jalan - jalan, <span class="text-primary">Mari</span> Travel</h1>
+                    <p>
+                        Yuk kita jalan, melepas penat, mengisi hari bersama teman teman. jadi, nari travel
+                    </p class="lead">
+
+                    <button class="btn btn-primary btn-lg">
+                    Yuk Berangkat
+                    </button>
+                </div>
+
+                <img class="img-fluid w-50 d-none d-sm-block" src="assets/images/header.svg" alt="header">
+                </div>
+            </div>
+    </section>
---
+++ Bootstrap Kelas Terbuka/component/navbar.php
@@ -0,0 +1,30 @@
+    <nav class="navbar navbar-expand-lg bg-dark navbar-dark fixed-top py-3">
+
+        <div class="container">
+
+        <a href="#" class="navbar-brand">Mari Travel</a>
+
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navmenu"
+            >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+            <div class="colappse navbar-collapse" id="navmenu">
+                <ul class="navbar-nav ms-auto">
+                    <li class="navbar-item">
+                        <a href="" class="nav-link">Service</a>
+                    </li>
+                    <li class="navbar-item">
+                        <a href="" class="nav-link">Contact</a>
+                    </li>
+                    <li class="navbar-item">
+                        <a href="" class="nav-link">About Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>    
